0398c846f46f068ced0faf6bcf6b187a8771a7b5
muhumbania <franklinmuhumbania@gmail.com>
2024-06-04 23:37:13 +0300

    Uploaded the serial code

--- index.py
+++ index.py
@@ -1,7 +1,32 @@
 import csv
+import serial
+import time
 
 # field names
 fields = ['Name', 'Branch', 'Year', 'CGPA']
+
+# set up the serial line
+ser = serial.Serial('COM5', 9600)
+time.sleep(2)
+
+# Read and record the data
+data =[]                       # empty list to store the data
+for i in range(50):
+    b = ser.readline()         # read a byte string
+    string_n = b.decode()  # decode byte string into Unicode  
+    string = string_n.rstrip() # remove \n and \r
+    flt = float(string)        # convert string to float
+    print(flt)
+    data.append(flt)           # add to the end of data list
+    time.sleep(0.1)            # wait (sleep) 0.1 seconds
+
+ser.close()
+
+
+# show the data
+
+for line in data:
+    print(line)
 
 # data rows of csv file
 rows = [['Nikhil', 'COE', '2', '9.0'],
